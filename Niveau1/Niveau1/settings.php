45a5e6d3626b9573daae9a0396c95a6aa9a500ec
Anael Toula <anael.toula@gmail.com>
2024-05-28 16:52:02 +0200

    new seeting.php

--- Niveau1/Niveau1/settings.php
+++ Niveau1/Niveau1/settings.php
@@ -1,47 +1,49 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Paramètres</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Paramètres</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
     <?php
-       include "menu.php"
-       ?>
-        <div id="wrapper" class='profile'>
+    include "menu.php"
+        ?>
+    <div id="wrapper" class='profile'>
 
 
-            <aside>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez les informations de l'utilisatrice
-                        n° <?php echo intval($_GET['user_id']) ?></p>
+        <aside>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez les informations de l'utilisatrice
+                    n° <?php echo intval($_GET['user_id']) ?></p>
 
-                </section>
-            </aside>
-            <main>
-                <?php
-                /**
-                 * Etape 1: Les paramètres concernent une utilisatrice en particulier
-                 * La première étape est donc de trouver quel est l'id de l'utilisatrice
-                 * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
-                 * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
-                 * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
-                 */
-                $userId = intval($_GET['user_id']);
+            </section>
+        </aside>
+        <main>
+            <?php
+            /**
+             * Etape 1: Les paramètres concernent une utilisatrice en particulier
+             * La première étape est donc de trouver quel est l'id de l'utilisatrice
+             * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
+             * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
+             * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
+             */
+            $userId = intval($_GET['user_id']);
 
-                /**
-                 * Etape 2: se connecter à la base de donnée
-                 */
-                include "donnees.php";
+            /**
+             * Etape 2: se connecter à la base de donnée
+             */
+            include "donnees.php";
 
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */
-                $laQuestionEnSql = "
+            /**
+             * Etape 3: récupérer le nom de l'utilisateur
+             */
+            $laQuestionEnSql = "
                     SELECT users.*, 
                     count(DISTINCT posts.id) as totalpost, 
                     count(DISTINCT given.post_id) as totalgiven, 
@@ -53,36 +55,40 @@
                     WHERE users.id = '$userId' 
                     GROUP BY users.id
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
-                $user = $lesInformations->fetch_assoc();
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if (!$lesInformations) {
+                echo ("Échec de la requete : " . $mysqli->error);
+            }
+            $user = $lesInformations->fetch_assoc();
 
-                /**
-                 * Etape 4: à vous de jouer
-                 */
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
-                ?>                
-                <article class='parameters'>
-                    <h3>Mes paramètres</h3>
-                    <dl>
-                        <dt>Pseudo</dt>
-                        <dd>Félicie</dd>
-                        <dt>Email</dt>
-                        <dd>felicie@test.org</dd>
-                        <dt>Nombre de message</dt>
-                        <dd>42</dd>
-                        <dt>Nombre de "J'aime" donnés </dt>
-                        <dd>12</dd>
-                        <dt>Nombre de "J'aime" reçus</dt>
-                        <dd>53</dd>
-                    </dl>
+            /**
+             * Etape 4: à vous de jouer
+             */
+            //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
+            // echo "<pre>" . print_r($user, 1) . "</pre>";
+            ?>
+            <?php
 
-                </article>
-            </main>
-        </div>
-    </body>
+
+            ?>
+            <article class='parameters'>
+                <h3>Mes paramètres</h3>
+                <dl>
+                    <dt>Pseudo</dt>
+                    <dd> <?php echo $user['alias'] ?> </dd>
+                    <dt>Email</dt>
+                    <dd> <?php echo $user['email'] ?> </dd>
+                    <dt>Nombre de message</dt>
+                    <dd><?php echo $user['totalpost'] ?></dd>
+                    <dt>Nombre de "J'aime" donnés </dt>
+                    <dd><?php echo $user['totalgiven'] ?></dd>
+                    <dt>Nombre de "J'aime" reçus</dt>
+                    <dd><?php echo $user['totalrecieved'] ?></dd>
+                </dl>
+
+            </article>
+        </main>
+    </div>
+</body>
+
 </html>
