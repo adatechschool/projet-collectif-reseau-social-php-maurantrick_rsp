c4f9edc0c6f9e6986747eadb1a186aafe35fdec8
Katshual <139873320+Katshual@users.noreply.github.com>
2024-05-28 18:12:54 +0200

    28/05/24 à 18H11 refactoring du fichier admin.php avec réalisation des étapes @todo 3 et 5

--- Niveau1/Niveau1/admin.php
+++ Niveau1/Niveau1/admin.php
@@ -49,10 +49,10 @@ include "menu.php"
                 echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <article>
-                    <h3>#chaussette</h3>
-                    <p>id:321</p>
+                    <h3><?php echo $tag['id'] ?></h3>
+                    <p><?php echo $tag['label']?></p>
                     <nav>
-                        <a href="tags.php?tag_id=321">Messages</a>
+                        <a href="tags.php?tag_id=<?php echo $tag['id'] ?>">Messages</a>
                     </nav>
                 </article>
             <?php } ?>
@@ -80,14 +80,14 @@ include "menu.php"
                 echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <article>
-                    <h3>Alexandra</h3>
-                    <p>id:123</p>
+                    <h3><?php echo $tag['alias'] ?></h3>
+                    <p><?php echo $tag['id'] ?></p>
                     <nav>
-                        <a href="wall.php?user_id=123">Mur</a>
-                        | <a href="feed.php?user_id=123">Flux</a>
-                        | <a href="settings.php?user_id=123">Paramètres</a>
-                        | <a href="followers.php?user_id=123">Suiveurs</a>
-                        | <a href="subscriptions.php?user_id=123">Abonnements</a>
+                        <a href="wall.php?user_id=<?php echo $tag['id'] ?><">Mur</a>
+                        | <a href="feed.php?user_id=<?php echo $tag['id'] ?><">Flux</a>
+                        | <a href="settings.php?user_id=<?php echo $tag['id'] ?><">Paramètres</a>
+                        | <a href="followers.php?user_id=<?php echo $tag['id'] ?><">Suiveurs</a>
+                        | <a href="subscriptions.php?user_id=<?php echo $tag['id'] ?><">Abonnements</a>
                     </nav>
                 </article>
             <?php } ?>
