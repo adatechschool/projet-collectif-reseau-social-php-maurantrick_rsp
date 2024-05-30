cca5b3de5c1a2898bcd8b46f60dc2d9ac6d5d2ed
Katshual <139873320+Katshual@users.noreply.github.com>
2024-05-30 18:33:24 +0200

    30/05/24 18h27 mise à jour des fichiers login.php et registration.php avec l'instruction include et ajout des copies des fichiers du dossier niveau 1 (admin, donnees, feed, followers, menu, news, settings, subscriptions, tags et wall)

---
+++ Niveau2/Niveau2/feed.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ReSoC - Flux</title>         
+        <meta name="author" content="Julien Falconnet">
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+    <?php
+       include "menu.php"
+       ?>
+        <div id="wrapper">
+            <?php
+            /**
+             * Cette page est TRES similaire à wall.php. 
+             * Vous avez sensiblement à y faire la meme chose.
+             * Il y a un seul point qui change c'est la requete sql.
+             */
+            /**
+             * Etape 1: Le mur concerne un utilisateur en particulier
+             */
+            $userId = intval($_GET['user_id']);
+            ?>
+           
+            
+             <!-- * Etape 2: se connecter à la base de donnée -->
+             
+            <?php
+       include "donnees.php"
+       ?>
+
+       <aside>
+                <?php
+                /**
+                 * Etape 3: récupérer le nom de l'utilisateur
+                 */
+                $laQuestionEnSql = "SELECT * FROM `users` WHERE id= '$userId' ";
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                $user = $lesInformations->fetch_assoc();
+                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
+                // echo "<pre>" . print_r($user, 1) . "</pre>";
+                ?>
+                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <section>
+                    <h3>Présentation</h3>
+                    <p>Sur cette page vous trouverez tous les message des utilisatrices
+                        auxquel est abonnée l'utilisatrice 
+                        : <?php echo $user['alias'] ?>
+                    </p>
+
+                </section>
+            </aside>
+            <main>
+                <?php
+                /**
+                 * Etape 3: récupérer tous les messages des abonnements
+                 */
+                $laQuestionEnSql = "
+                    SELECT posts.content,
+                    posts.created,
+                    users.id as id,
+                    users.alias as author_name,  
+                    count(likes.id) as like_number,  
+                    GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    FROM followers 
+                    JOIN users ON users.id=followers.followed_user_id
+                    JOIN posts ON posts.user_id=users.id
+                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+                    LEFT JOIN likes      ON likes.post_id  = posts.id 
+                    WHERE followers.following_user_id='$userId' 
+                    GROUP BY posts.id
+                    ORDER BY posts.created DESC  
+                    ";
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                if ( ! $lesInformations)
+                {
+                    echo("Échec de la requete : " . $mysqli->error);
+                }
+
+                /**
+                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+                 * A vous de retrouver comment faire la boucle while de parcours...
+                 */
+                while ($post = $lesInformations->fetch_assoc())
+                {
+                    
+                ?>         
+                
+                <article>
+                    <h3>
+                        <time datetime='2020-02-01 11:12:13'><?php echo $post['created']?></time>
+                    </h3>
+                    
+                    <a href="wall.php?user_id=<?php echo $post['id'];?>"><address><?php echo htmlspecialchars($post['author_name']) ?></address></a>
+                    <div>     
+                           
+                    <!-- // echo "<pre>" . print_r($post['id'], 1) . "</pre>";
+                    // echo "<pre>" . print_r($post, 1) . "</pre>" -->
+                    
+                        <p><?php echo $post['content'] ?></p>
+                    </div>                                            
+                    <footer>
+                        <small>♥ <?php echo $post['like_number'] ?></small>
+                        <?php
+                                $exploded = explode(',', $post['taglist']);
+                                foreach($exploded as $tag) {
+                                    echo '<a href="">#'.$tag.'</a>, ';
+                                }
+                            ?>
+                            
+                        
+                    </footer>
+                </article>
+                <?php
+                }
+                // et de pas oublier de fermer ici vote while
+                ?>
+
+
+            </main>
+        </div>
+    </body>
+</html>
---
+++ Niveau2/Niveau2/tags.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ReSoC - Les message par mot-clé</title> 
+        <meta name="author" content="Julien Falconnet">
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+    <?php
+       include "menu.php"
+       ?>
+        <div id="wrapper">
+            <?php
+            /**
+             * Cette page est similaire à wall.php ou feed.php 
+             * mais elle porte sur les mots-clés (tags)
+             */
+            /**
+             * Etape 1: Le mur concerne un mot-clé en particulier
+             */
+            $tagId = intval($_GET['tag_id']);
+            ?>
+            <?php
+            /**
+             * Etape 2: se connecter à la base de donnée
+             */
+            include "donnees.php"
+            ?>
+
+            <aside>
+                <?php
+                /**
+                 * Etape 3: récupérer le nom du mot-clé
+                 */
+                $laQuestionEnSql = "SELECT * FROM tags WHERE id= '$tagId' ";
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                $tag = $lesInformations->fetch_assoc();
+                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
+                
+                ?>
+                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <section>
+                    <h3>Présentation</h3>
+                    <p>Sur cette page vous trouverez les derniers messages comportant
+                        le mot-clé:
+                        <?php echo $tag['label'] ?>
+                    </p>
+
+                </section>
+            </aside>
+            <main>
+                <?php
+                /**
+                 * Etape 3: récupérer tous les messages avec un mot clé donné
+                 */
+                $laQuestionEnSql = "
+                    SELECT posts.content,
+                    posts.created,
+                    users.id as id,
+                    users.alias as author_name,  
+                    count(likes.id) as like_number,  
+                    GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    FROM posts_tags as filter 
+                    JOIN posts ON posts.id=filter.post_id
+                    JOIN users ON users.id=posts.user_id
+                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+                    LEFT JOIN likes      ON likes.post_id  = posts.id 
+                    WHERE filter.tag_id = '$tagId' 
+                    GROUP BY posts.id
+                    ORDER BY posts.created DESC  
+                    ";
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                if ( ! $lesInformations)
+                {
+                    echo("Échec de la requete : " . $mysqli->error);
+                }
+
+                /**
+                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+                 */
+                while ($post = $lesInformations->fetch_assoc())
+                {
+
+                    // echo "<pre>" . print_r($post, 1) . "</pre>";
+                    ?>                
+                    <article>
+                        <h3>
+                            <time datetime='2020-02-01 11:12:13' ><?php echo $post['created'] ?></time>
+                        </h3>
+                            <address><a href="wall.php?user_id=<?php echo $post['id'];?>"><?php  echo htmlspecialchars($post['author_name']) ?></a></address>
+                        <div>
+                            <p><?php echo $post['content'] ?></p>
+                        </div>                                            
+                        <footer>
+                            <small>♥ <?php echo $post['like_number'] ?></small>
+                            <a href="">#<?php echo $post['taglist'] ?></a>,
+                            
+                        </footer>
+                    </article>
+                <?php } ?>
+
+
+            </main>
+        </div>
+    </body>
+</html>
