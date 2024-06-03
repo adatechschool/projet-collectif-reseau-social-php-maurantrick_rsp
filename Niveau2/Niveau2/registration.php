d05868df9064c843d89f3fe3b152855517c8963e
Anael Toula <anael.toula@gmail.com>
2024-06-03 11:22:11 +0200

    03/06 new Login & registration

--- Niveau2/Niveau2/registration.php
+++ Niveau2/Niveau2/registration.php
@@ -31,7 +31,7 @@
                         // on ne fait ce qui suit que si un formulaire a été soumis.
                         // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
                         // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
-                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                    //    w< j,  b n
                         // et complétez le code ci dessous en remplaçant les ???
                         $new_email = $_POST['email'];
                         $new_alias = $_POST['pseudo'];
